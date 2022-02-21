702b20c62513d7b4570f088ceb11109e7632a179
Danny McClain <dannyxmcclain@gmail.com>
2022-02-21 14:48:39 -0600

    just getting some logic worked out

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+let tallllies = [
+        { emoji: '🛸', title: 'UFO sightings', count: 1, id: 'a' },
+        { emoji: '👽', title: 'Alien abductions', count: 7, id: 'b' },
+]
+
+const increment = function(data) {
+    let count = data.count;
+    let emoji = data.emoji;
+    let id = data.id;
+    let title = data.title;
+    let newTallllies = Array.from(tallllies);
+    newTallllies.forEach(function(data){
+        if (data.id === id){
+            let higherCount = (data.count + 1);
+            data.count = higherCount;
+            console.log(data.emoji, data.count);
+        }
+        tallllies = newTallllies;
+    })
+}
+</script>
+
+<h1>Tally</h1>
+
+<section>
+    {#each tallllies as talllly}
+        <h2>{talllly.emoji}</h2>
+        <h3>{talllly.title} <small>({talllly.id})</small></h3>
+        <p>{talllly.count}</p>
+        <button on:click={() => increment(talllly)}>+</button>
+    {/each}
+</section>
+
+<style>
+
+</style>
